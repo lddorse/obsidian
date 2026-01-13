64170b8292dce53e70ac7c2b9db9b9f3c8a7a458
lddorse <42456504+lddorse@users.noreply.github.com>
2026-01-13 11:39:53 -0600

    Create businessInfo.js

---
+++ src/data/businessInfo.js
@@ -0,0 +1,27 @@
+export const hours = {
+  title: 'HOURS',
+  schedule: [
+    { day: 'Monday - Thursday', time: '7:00 AM - 10:00 PM' },
+    { day: 'Friday', time: '7:00 AM - 12:00 AM' },
+    { day: 'Saturday', time: '8:00 AM - 12:00 AM' },
+    { day: 'Sunday', time: '8:00 AM - 8:00 PM' }
+  ]
+};
+
+export const locationInfo = {
+  title: 'LOCATION & ABOUT',
+  address: '123 Main Street, Downtown District',
+  city: 'Your City, ST 12345',
+  description: 'Obsidian is a specialty coffee roastery and craft cocktail bar located in the heart of downtown. We roast our own beans and craft cocktails that pair perfectly with our coffee selection.',
+  directions: {
+    title: 'FINDING US',
+    note: 'ENTRANCE IN ALLEY',
+    steps: [
+      'Located on Main Street between 1st and 2nd Avenue',
+      'Look for the obsidian stone marker on the building',
+      'Enter through the alley on the west side',
+      'Follow the amber lights to our door',
+      'Ring bell if door is closed'
+    ]
+  }
+};
